917b55adb209d15924755387ac4e02f231a147b9
Pedro Sousa <sketchpunk@gmail.com>
2022-07-03 02:34:00 -0400

    Bezier Cubic Spline

--- src/bezier/BezierCubicSpline.ts
+++ src/bezier/BezierCubicSpline.ts
@@ -1,16 +1,16 @@
 // #region IMPORTS
 import { Spline }           from '../Spline';
-import BezierQuad           from './BezierQuad';
+import BezierCubic          from './BezierCubic';
 
 import type { TVec3 }       from 'oito';
 import { Maths }            from 'oito';
 // #endregion
 
-export default class BezierQuadSpline extends Spline{
+export default class BezierCubicSpline extends Spline{
     // #region MANAGE POINTS
     add( pos: TVec3 ) : this{
         super.add( pos );
-        this._curveCnt = Math.max( 0, Math.floor( (this._pointCnt - 1) / 2 ) );
+        this._curveCnt = Math.max( 0, Math.floor( (this._pointCnt - 1) / 3 ) );
         return this;
     }
     // #endregion
@@ -27,14 +27,14 @@ export default class BezierQuadSpline extends Spline{
         if( t > 1 )      t = 1;
         else if( t < 0 ) t = 0;
 
-        const p               = this.points;
-        const [ a, b, c, tt ] = ( !this._isLoop )?
+        const p                  = this.points;
+        const [ a, b, c, d, tt ] = ( !this._isLoop )?
             this._computeCurveIdx( t ) :
             this._computeLoopIdx( t ) ;
 
-        if( pos )   BezierQuad.at(    p[a].pos, p[b].pos, p[c].pos, tt, pos );
-        if( dxdy )  BezierQuad.dxdy(  p[a].pos, p[b].pos, p[c].pos, tt, dxdy );
-        if( dxdy2 ) BezierQuad.dxdy2( p[a].pos, p[b].pos, p[c].pos, tt, dxdy2 );
+        if( pos )   BezierCubic.at(    p[a].pos, p[b].pos, p[c].pos, p[d].pos, tt, pos );
+        if( dxdy )  BezierCubic.dxdy(  p[a].pos, p[b].pos, p[c].pos, p[d].pos, tt, dxdy );
+        if( dxdy2 ) BezierCubic.dxdy2( p[a].pos, p[b].pos, p[c].pos, p[d].pos, tt, dxdy2 );
     }
 
     atCurve( cIdx: number, t: number, pos ?: TVec3, dxdy ?: TVec3, dxdy2 ?: TVec3 ): void{
@@ -42,49 +42,56 @@ export default class BezierQuadSpline extends Spline{
         else if( t < 0 ) t = 0;
 
         const p = this.points;
-        const a = cIdx * 2;
+        const a = cIdx * 3;
         const b = Maths.mod( a + 1, this._pointCnt );
         const c = Maths.mod( a + 2, this._pointCnt );
+        const d = Maths.mod( a + 3, this._pointCnt );
 
-        if( pos )   BezierQuad.at(    p[a].pos, p[b].pos, p[c].pos, t, pos );
-        if( dxdy )  BezierQuad.dxdy(  p[a].pos, p[b].pos, p[c].pos, t, dxdy );
-        if( dxdy2 ) BezierQuad.dxdy2( p[a].pos, p[b].pos, p[c].pos, t, dxdy2 );
+        if( pos )   BezierCubic.at(    p[a].pos, p[b].pos, p[c].pos, p[d].pos, t, pos );
+        if( dxdy )  BezierCubic.dxdy(  p[a].pos, p[b].pos, p[c].pos, p[d].pos, t, dxdy );
+        if( dxdy2 ) BezierCubic.dxdy2( p[a].pos, p[b].pos, p[c].pos, p[d].pos, t, dxdy2 );
     }
     // #endregion
 
     // #region HELPERS
-    /** Compute the point indices for open spline : Return: [ aIdx, bIdx, cIdx, t ] */
-    _computeCurveIdx( t: number ) : [number,number,number,number]{
+    /** Compute the point indices for open spline : Return: [ aIdx, bIdx, cIdx, dIdx, t ] */
+    _computeCurveIdx( t: number ) : [number,number,number,number,number]{
         let i, tt;
 
         if( t != 1 ){
             tt  = t * this._curveCnt;   // Using Curve count as a way to get the Index and the remainder is the T of the curve
             i   = tt | 0;	            // BitwiseOR 0 same op as Floor
             tt -= i;		            // Strip out the whole number to get the decimal to be used for the T of curve ( FRACT )
-            i  *= 2;                    // Every 2 points Plus one back counts as 1 bezier quad curve
+            i  *= 3;                    // Every 3 points Plus one back counts as 1 bezier cubic curve
         }else{
-            i	= ( this._curveCnt - 1 ) * 2;
+            i	= ( this._curveCnt - 1 ) * 3;
             tt	= 1;                        // The end of the final curve.
         }
 
-        return [ i, i+1, i+2, tt ];
+        return [ i, i+1, i+2, i+3, tt ];
     }
 
-    /** Compute the point indices for closed spline : Return: [ aIdx, bIdx, cIdx, t ] */
-    _computeLoopIdx( t: number ) : [number,number,number,number]{
+    /** Compute the point indices for closed spline : Return: [ aIdx, bIdx, cIdx, dIdx, t ] */
+    _computeLoopIdx( t: number ) : [number,number,number,number,number]{
         let i, tt;
 
         if( t != 1 ){
             tt  = t * ( this._curveCnt + 1 );   // Using Curve count as a way to get the Index and the remainder is the T of the curve
             i   = tt | 0;	            // BitwiseOR 0 same op as Floor
             tt -= i;		            // Strip out the whole number to get the decimal to be used for the T of curve ( FRACT )
-            i  *= 2;                    // Every 2 points Plus one back counts as 1 bezier quad curve
+            i  *= 3;                    // Every 3 points Plus one back counts as 1 bezier cubic curve
         }else{
-            i	= this._pointCnt - 2;
+            i	= this._pointCnt - 3;
             tt	= 1;                    // The end of the final curve.
         }
 
-        return [ i, Maths.mod( i+1, this._pointCnt ), Maths.mod( i+2, this._pointCnt ), tt ];
+        return [ 
+            i, 
+            Maths.mod( i+1, this._pointCnt ), 
+            Maths.mod( i+2, this._pointCnt ), 
+            Maths.mod( i+3, this._pointCnt ), 
+            tt
+        ];
     }
     // #endregion
 }
